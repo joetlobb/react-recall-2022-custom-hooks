e42132c052da5b8ff0ab8706a011edb76056b12b
Gulyapas Poonkawinsiri <gulyapas.p@gmail.com>
2022-05-24 10:00:17 +0700

    using max's approaches on custom hook in app.js for fetching tasks from firebase

--- src/App.js
+++ src/App.js
@@ -2,52 +2,29 @@ import React, { useEffect, useState } from "react";
 
 import Tasks from "./components/Tasks/Tasks";
 import NewTask from "./components/NewTask/NewTask";
-import useHTTP from "./components/Hooks/use-http";
+import useHttp from "./components/hooks/use-http";
 
 function App() {
-  // const [isLoading, setIsLoading] = useState(false);
-  // const [error, setError] = useState(null);
   const [tasks, setTasks] = useState([]);
 
-  const fetchTaskHandler = (tasksList) => {
-    setTasks(tasksList);
-  };
-
-  const { onTaskRequest, isLoading, error } = useHTTP(null, fetchTaskHandler);
-
-  // const fetchTasks = async (taskText) => {
-  //   setIsLoading(true);
-  //   setError(null);
-  //   try {
-  //     const response = await fetch(
-  //       "https://react-http-d578e-default-rtdb.firebaseio.com/tasks.json",
-  //       {}
-  //     );
-
-  //     if (!response.ok) {
-  //       throw new Error("Request failed!");
-  //     }
-
-  //     const data = await response.json();
-
-  //     const loadedTasks = [];
-
-  //     for (const taskKey in data) {
-  //       loadedTasks.push({ id: taskKey, text: data[taskKey].text });
-  //     }
-
-  //     setTasks(loadedTasks);
-  //   } catch (err) {
-  //     setError(err.message || "Something went wrong!");
-  //   }
-  //   setIsLoading(false);
-  // };
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp();
 
   useEffect(() => {
-    if (tasks.length === 0) {
-      onTaskRequest();
-    }
-  }, [tasks, onTaskRequest]);
+    const transformTasks = (tasksObject) => {
+      const loadedTasks = [];
+      for (const taskKey in tasksObject) {
+        loadedTasks.push({ id: taskKey, text: tasksObject[taskKey].text });
+      }
+      setTasks(loadedTasks);
+    };
+
+    fetchTasks(
+      {
+        url: "https://react-http-d578e-default-rtdb.firebaseio.com/tasks.json",
+      },
+      transformTasks
+    );
+  }, [fetchTasks]);
 
   const taskAddHandler = (task) => {
     setTasks((prevTasks) => prevTasks.concat(task));
@@ -57,11 +34,10 @@ function App() {
     <React.Fragment>
       <NewTask onAddTask={taskAddHandler} />
       <Tasks
-        // items={response.tasksList}
         items={tasks}
         loading={isLoading}
         error={error}
-        // onFetch={fetchTasks}
+        onFetch={fetchTasks}
       />
     </React.Fragment>
   );
--- src/components/Hooks/use-http.js
+++ src/components/Hooks/use-http.js
@@ -1,65 +1,37 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 
-const useHTTP = (post = null, onConcat = null) => {
+const useHttp = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
-  let createdTask = {};
-  let loadedTasks = [];
 
-  const taskHandler = async (taskText) => {
-    console.log("TaskHandler RUNNING")
+  const sendRequest = useCallback(async (requestConfig, applyData) => {
     setIsLoading(true);
     setError(null);
 
-    let content = {};
-    if (post) {
-      content = {
-        method: "POST",
-        body: JSON.stringify({ text: taskText }),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      };
-    }
-
     try {
-      const response = await fetch(
-        "https://react-http-d578e-default-rtdb.firebaseio.com/tasks.json",
-        content
-      );
+      const response = await fetch(requestConfig.url, {
+        method: requestConfig.method ? requestConfig.method : "GET",
+        headers: requestConfig.headers ? requestConfig.headers : {},
+        body: requestConfig.body ? JSON.stringify(requestConfig.body) : null,
+      });
 
       if (!response.ok) {
         throw new Error("Request failed!");
-      } else {
       }
 
       const data = await response.json();
-
-      if (post) {
-        const generatedId = data.name; // firebase-specific => "name" contains generated id
-        createdTask = { id: generatedId, text: taskText };
-        onConcat(createdTask);
-      } else {
-        loadedTasks = [];
-
-        for (const taskKey in data) {
-          loadedTasks.push({ id: taskKey, text: data[taskKey].text });
-        }
-        onConcat(loadedTasks)
-      }
+      applyData(data);
     } catch (err) {
       setError(err.message || "Something went wrong!");
     }
     setIsLoading(false);
-  };
+  }, []);
 
   return {
-    isLoading: isLoading,
-    hasError: error,
-    onTaskRequest: taskHandler,
-    newTask: createdTask,
-    tasksList: loadedTasks,
+    isLoading,
+    error,
+    sendRequest,
   };
 };
 
-export default useHTTP;
+export default useHttp;
